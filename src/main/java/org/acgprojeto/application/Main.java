86986513cba148e79a596e65363901d110a45867
Anderson da Silva Santos <anderson.ss1090@gmail.com>
2024-08-23 15:16:24 -0300

    Adição do padrão estate para a classe pedido

--- src/main/java/org/acgprojeto/application/Main.java
+++ src/main/java/org/acgprojeto/application/Main.java
@@ -1,35 +1,49 @@
 package org.acgprojeto.application;
 
-import org.acgprojeto.dao.ClienteDAO;
-import org.acgprojeto.dao.ProdutoDAO;
+import org.acgprojeto.dao.*;
 import org.acgprojeto.dao.impl.ClienteDAOImpl;
 import org.acgprojeto.dao.impl.ProdutoDAOImpl;
 import org.acgprojeto.db.DB;
 import org.acgprojeto.dto.ClienteDTO;
+import org.acgprojeto.dto.PedidoDTO;
 import org.acgprojeto.dto.ProdutoDTO;
+import org.acgprojeto.dto.ServicoDTO;
+import org.acgprojeto.model.entidades.Pedido;
 import org.acgprojeto.model.enums.Categoria;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
+import java.util.List;
 
 public class Main  {
 
     public static void main(String[] args) {
-        Connection connection = DB.getConexao();
-
-        ClienteDAO clienteDAO = new ClienteDAOImpl(connection);
-        ProdutoDAO produtoDAO = new ProdutoDAOImpl(connection);
-        //ClienteDTO clienteDTO = new ClienteDTO(null, "opa", "gaag", "321321");
-        ProdutoDTO produtoDTO = ProdutoDTO.ProdutoDTOBuilder.aProdutoDTO()
-                .nomeProduto("algo")
-                .categoria(Categoria.PECA)
-                .quantidadeEstoque(3)
-                .preco(new BigDecimal(199.99))
-                .build();
-
-        //clienteDAO.inserirCliente(clienteDTO);
-
-        produtoDAO.inserirProduto(produtoDTO);
+//        Connection connection = DB.getConexao();
+//
+//        ClienteDAO clienteDAO = new ClienteDAOImpl(connection);
+//        ProdutoDAO produtoDAO = new ProdutoDAOImpl(connection);
+//        //ClienteDTO clienteDTO = new ClienteDTO(null, "opa", "gaag", "321321");
+//        ProdutoDTO produtoDTO = ProdutoDTO.ProdutoDTOBuilder.aProdutoDTO()
+//                .nomeProduto("algo")
+//                .categoria(Categoria.PECA)
+//                .quantidadeEstoque(3)
+//                .preco(new BigDecimal(199.99))
+//                .build();
+//
+//        //clienteDAO.inserirCliente(clienteDTO);
+//
+//        produtoDAO.inserirProduto(produtoDTO);
+        ServicoDAO servicoDAO = DAOFactory.criarServicoDAO();
+        PedidoDAO pedidoDAO = DAOFactory.criarPedidoDAO();
+        PedidoDTO pedidoDTO = pedidoDAO.buscarPedidoPorId(1);
+        Pedido pedido = new Pedido(pedidoDTO);
+        pedido.getEstado().setPedidoDTO(pedido.toDTO());
+        pedido.gerarRelatorio();
+//        List<ServicoDTO> servicos = servicoDAO.listarServicosPorPedido(pedidoDTO);
+//
+//        for (ServicoDTO servico : servicos) {
+//            System.out.println(servico);
+//        }
 
         DB.fecharConexao();
     }
